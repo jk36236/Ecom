2b92256c84bc3e4116f76f2627daa63013ad39c7
jatin <jatinkalia08@gmail.com>
2024-02-13 13:20:37 +0530

    created products API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,95 @@
-exports.getAllProducts=(req,res)=>{
+const Product= require('../models/productModel');
 
-  res.status(200).json({message:"Route is working fine"})
+//create product-ADMIN --CREATE
+exports.createProduct = async(req,res,next)=>{
+  const product= await Product.create(req.body);
+  res.status(201).json({
+    success:true,
+    product
+  })
+}
+
+
+
+//get all products --READ
+exports.getAllProducts=async(req,res)=>{
+
+  const products= await Product.find();//will find all the products
+
+  res.status(200).json({
+    success:true,
+    products
+  })
+}
+
+//get product details- SINGLE PRODUCT
+exports.getProductDetails= async(req,res,next)=>{
+  //find pdt by id
+  const product= await Product.findById(req.params.id);
+
+//if not found
+if(!product){
+  return res.status(500).json({
+    success:false,
+    message:"product not found"
+  });
+}
+// if found-send pdt details
+res.status(200).json({
+  success:true,
+  product
+})
+}
+
+//update products--UPDATE--ADMIN
+exports.updateProduct= async(req,res,next)=>{
+ let product= await Product.findById(req.params.id);//find product by id which has to be updated, grab id from params
+
+ //if product not found
+ if(!product){
+  return res.status(500).json({
+    success:false,
+    message:"Product not found"
+  })
+ }
+
+ //if found update it
+ //id,update,options
+ product= await Product.findByIdAndUpdate(req.params.id,req.body,{
+  new:true,
+  runValidators:true,
+  useFindAndModify:false
+});
+
+//product has been updated ,return response with updated product
+res.status(200).json({
+  success:true,
+  product //updated product
+});
+
+}
+
+
+
+
+//DELETE PRODUCT -- ADMIN
+exports.deleteProduct = async(req,res,next)=>{
+  //find the product
+ const product= await Product.findById(req.params.id);
+
+ //if not found
+ if(!product){
+  return res.status(500).json({
+    success:false,
+    message:"product not found"
+  });
+ }
+
+ //if found delete it
+ await product.remove();
+
+ res.status(200).json({
+  success:true,
+  message:"Product deleted successfully"
+ });
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,19 @@
 const express=require('express');
 const router= express.Router();
-const { getAllProducts } = require('../controllers/productController');
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
 
+//get all products route --READ
 router.route('/products').get(getAllProducts);
+//create product route-- ye admin route hai--CREATE
+router.route('/product/new').post(createProduct);
+
+//update product route--UPDATE --ADMIN
+router.route('/product/:id').put(updateProduct);
+
+//delete product route--DELETE -- ADMIN
+router.route('/product/:id').delete(deleteProduct);
+
+//get singlepdt details
+router.route('/product/:id').get(getProductDetails);
 
 module.exports=router;
