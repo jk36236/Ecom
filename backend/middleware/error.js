6c46691b35b2d170902ec3b8d811388af2a9c4db
jatin <jatinkalia08@gmail.com>
2024-03-07 09:49:51 +0530

    Auth-Error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,4 +11,30 @@ if(err.name === "CastError"){
   const message=  `Resource not found. Invalid ${err.path}`;
   err=new ErrorHandler(message,400);//400-bad request
 }
+
+//mongoose duplicate key error
+if(err.code === 11000){
+  const message = `Duplicate ${Object.keys(err.KeyValue)} Entered`;
+  console.log(message);
+  err= new ErrorHandler(message,400);
+}
+
+//Wrong JWT error
+if(err.name === "JsonWebTokenError"){
+  const message = `Json web Token is invalid,Try again`;
+  console.log(message);
+  err= new ErrorHandler(message,400);
+} 
+
+//JWT Expire error
+if(err.name === "TokenExpiredError"){
+  const message = `Json web Token is expired,Try again`;
+  console.log(message);
+  err= new ErrorHandler(message,400);
+} 
+
+res.status(err.statusCode).json({
+  success:false,
+  message:err.message
+});
 }
