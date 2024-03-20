188ad4e6ec260dc501601c5aba52fdf1ada5cf41
jatin <jatinkalia08@gmail.com>
2024-03-20 11:59:36 +0530

    Header-customizing

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -4,57 +4,58 @@ import logo from "../../../images/logo.png";
 import { FaSearch, FaShoppingCart} from 'react-icons/fa';
 import { BiSolidUserRectangle } from "react-icons/bi";
 
-const Header = () => {
-  return (
-   <ReactNavbar
-   burgerColorHover="#a62d24"
+const options={
+  burgerColorHover:"#a62d24",
+   logo,
+   logoWidth:"20vmax",
+   logoHoverSize:"10px",
+   logoHoverColor:"#eb4034",
 
-   logo={logo}
-   logoWidth="20vmax"
-   logoHoverSize="10px"
-   logoHoverColor="#eb4034"
+   navColor1:"white",
 
-   navColor1="white"
+   link1Text:"Home",
+   link2Text:"Product",
+   link3Text:"Contact",
+   link4Text:"About",
 
-   link1Text="Home"
-   link2Text="Product"
-   link3Text="Contact"
-   link4Text="About"
+   link1Url:"/",
+   link2Url:"/product",
+   link3Url:"/contact",
+   link4Url:"/about",
 
-   link1Url="/"
-   link2Url="/product"
-   link3Url="/contact"
-   link4Url="/about"
+   link1Size:"1.3vmax",
+   link1Color:"rgba(35,35,35,0.8)",
 
-   link1Size="1.3vmax"
-   link1Color="rgba(35,35,35,0.8)"
+   nav1justifyContent:"flex-end",
+   nav2justifyContent:"flex-end",
+   nav3justifyContent:"flex-start",
+   nav4justifyContent:"flex-start",
 
-   nav1justifyContent="flex-end"
-   nav2justifyContent="flex-end"
-   nav3justifyContent="flex-start"
-   nav4justifyContent="flex-start"
-
-   link1ColorHover="#eb4034"
-   link1Margin="1vmax"
+   link1ColorHover:"#eb4034",
+   link1Margin:"1vmax",
    
    
 //icons
-   searchIcon={true}
-   SearchIconElement={FaSearch}
-   cartIcon={true}
-   CartIconElement={ FaShoppingCart}
-   profileIcon={true}
-   ProfileIconElement={BiSolidUserRectangle}
+   searchIcon:true,
+   SearchIconElement:FaSearch,
+   cartIcon:true,
+   CartIconElement:FaShoppingCart,
+   profileIcon:true,
+   ProfileIconElement:BiSolidUserRectangle,
 
-   searchIconColor= "rgba(35, 35, 35,0.8)"
-   cartIconColor= "rgba(35, 35, 35,0.8)"
-   profileIconColor= "rgba(35, 35, 35,0.8)"
+   searchIconColor: "rgba(35, 35, 35,0.8)",
+   cartIconColor: "rgba(35, 35, 35,0.8)",
+   profileIconColor: "rgba(35, 35, 35,0.8)",
 
-   searchIconColorHover= "#eb4034"
-   cartIconColorHover= "#eb4034"
-   profileIconColorHover= "#eb4034"
-   cartIconMargin="1vmax"
-   />
+   searchIconColorHover: "#eb4034",
+   cartIconColorHover: "#eb4034",
+   profileIconColorHover: "#eb4034",
+   cartIconMargin:"1vmax",
+
+}
+const Header = () => {
+  return (
+   <ReactNavbar {...options} />
   );
 }
 
