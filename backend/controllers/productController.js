eb296c802dc6cb1d7bfa2a35cc0adfdff81608bd
jatin <jatinkalia08@gmail.com>
2024-04-07 11:53:08 +0530

    Filters-price filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,16 +27,21 @@ exports.getAllProducts=catchAsyncErrors(
 
 const apiFeature= new ApiFeatures(Product.find(),req.query)
 .search()
+
 .filter()
-.pagination(resultPerPage);
+let products= await apiFeature.query;
+const filteredProductsCount=products.length;
+apiFeature.pagination(resultPerPage);
+
   // const products= await Product.find();
-  const products= await apiFeature.query;
+   products= await apiFeature.query;
 
   res.status(200).json({
     success:true,
     products,
     productsCount,
     resultPerPage,
+    filteredProductsCount,
   })
 });
 
--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -6,13 +6,16 @@ import {clearErrors,getProduct} from "../../actions/productAction";
 import  Loader from '../layout/Loader/Loader';
 import ProductCard from '../Home/ProductCard';
 import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography"
 
 const Products = ({match}) => {
   const dispatch=useDispatch();
   
   const[currentPage,setCurrentPage]=useState(1);
+  const[price,setPrice]=useState([0,25000]);
  
-  const {products,loading,error,productsCount,resultPerPage} =useSelector(state=>state.products);
+  const {products,loading,error,productsCount,resultPerPage,filteredProductsCount} =useSelector(state=>state.products);
 
   
 const {keyword}=useParams();
@@ -21,12 +24,19 @@ const setCurrentPageNo=(e)=>{
   setCurrentPage(e);
 }
 
+const priceHandler=(event,newPrice)=>{
+  setPrice(newPrice);
+}
+
   useEffect(()=>{
-   dispatch(getProduct(keyword,currentPage));
-  },[dispatch,keyword,currentPage]);
+   dispatch(getProduct(keyword,currentPage,price));
+  },[dispatch,keyword,currentPage,price]);
+
+  let count =filteredProductsCount;
 
   return (
     <Fragment>
+      {/* products */}
       {loading ?( <Loader />) : (
         <Fragment>
           <h2 className='productsHeading'>Products</h2>
@@ -37,9 +47,22 @@ const setCurrentPageNo=(e)=>{
             )}
           </div>
 
+          {/* ---------filters---------- */}
+          <div className='filterBox'>
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby='range-slider'
+              min={0}
+              max={25000}
+            />
+          </div>
+
           {/* pagination */}
         {/* if resultsperpage < productsCount then only show pagination */}
-          {resultPerPage<productsCount && (
+          {resultPerPage<count && (
                 <div className='paginationBox'>
                 <Pagination 
                  activePage={currentPage}
