c0764d099456b8016c42e250bfc6167bfe35dca0
jatin <jatinkalia08@gmail.com>
2024-04-20 11:36:39 +0530

    Shipping component

---
+++ frontend/src/component/Cart/CheckoutSteps.js
@@ -0,0 +1,28 @@
+import { Typography } from '@material-ui/core';
+import React from 'react'
+
+const CheckoutSteps = ({activeStep}) => {
+
+const steps=[
+    {
+      label:<Typography>Shipping Details</Typography>,
+      icon:<LocalShippingIcon />,
+    },
+    {
+      label:<Typography>Confirm Order</Typography>,
+      icon:<LibraryAddChe,
+    },
+    {
+      label:,
+      icon:,
+    },
+];
+
+  return (
+<Fragment>
+
+</Fragment>
+    )
+}
+
+export default CheckoutSteps
